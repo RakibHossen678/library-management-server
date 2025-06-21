afaa8f873c9ab99aa408cd1add94b600718294a0
RakibHossen678 <rh491464@gmail.com>
2025-06-21 21:06:54 +0600

    Add server setup and application entry point; update package.json

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+
+let server: Server;
+const PORT = process.env.PORT || 7000;
+
+async function main() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://librarayMangement:qfYZjkZBvYZ4X9QE@cluster0.nbqp0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("Connected to MongoDB");
+    server = app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+  }
+}
+
+main();
